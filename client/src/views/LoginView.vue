2854df3305b18acd22de1a6fdcacd7d833507682
Yurii Rybachok <drummer2009@ukr.net>
2023-11-10 15:49:12 +0200

    function for submit form

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -4,6 +4,30 @@ import vm from '../main';
 
 const email = ref('');
 const isSubmitting = ref(false);
+
+const LoginOrCreate = async () => {
+  isSubmitting.value = true
+
+  if (!email.value) return alert("Please enter an email address")
+
+  const response = await fetch("http://localhost:5000/loginorcreate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: email.value
+    })
+  }).then(res => res.json())
+
+  if (response.success) {
+    vm.$toast.success(response.message)
+  } else {
+    vm.$toast.error(response.message)
+  }
+
+  isSubmitting.value = false
+}
 </script>
 
 <template>
@@ -11,7 +35,7 @@ const isSubmitting = ref(false);
     <section class="container mx-auto pt-16 pb-4 px-4">
       <h1 class="text-4xl mb-8 text-center font-black uppercase">Password-less Login</h1>
 
-      <form @submit.prevent="" class="bg-gray-700 max-w-md mx-auto rounded p-4">
+      <form @submit.prevent="LoginOrCreate" class="bg-gray-700 max-w-md mx-auto rounded p-4">
         <label class="block mb-2">
           <span class="block text-gray-400 mb-2"> Enter your email address </span>
           <input
