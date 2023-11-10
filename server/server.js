ee885cb2750a767e0a0e35682a559fbd1176e6b5
Yurii Rybachok <drummer2009@ukr.net>
2023-11-10 14:19:39 +0200

    stytch setup

--- server/server.js
+++ server/server.js
@@ -3,3 +3,11 @@ const stytch = require('stytch');
 const app = express()
 require('dotenv').config()
 
+//Stytch setup
+const client = new stytch.Client({
+    project_id: process.env.STYTCH_PID,
+    secret: process.env.STYTCH_SECRET,
+    env: process.env.STYTCH_ENV === "test"
+        ? stytch.envs.test
+        : stytch.envs.live
+})
