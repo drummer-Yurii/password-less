10c14cbaa95f8e6070bc07d609fd3658d82be0b8
Yurii Rybachok <drummer2009@ukr.net>
2023-11-10 10:14:14 +0200

    create Login page

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,8 @@ import './style.css'
 import App from './App.vue'
 import VueToast from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
+import router from './router'
 
-const vm = createApp(App).use(VueToast).mount('#app')
+const vm = createApp(App).use(router).use(VueToast).mount('#app')
 
 export default vm
