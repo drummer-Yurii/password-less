e5739c1b63ac64a87dad2b04ba701bfc6bda9007
Yurii Rybachok <drummer2009@ukr.net>
2023-11-10 10:30:48 +0200

    add data for form & create button

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,18 +1,38 @@
-<template>
-    <main class="login min-h-screen bg-gradient-to-br from-green-500 to-cyan-500">
-        <section class="container mx-auto pt-16 pb-4 px-4">
-            <h1 class="text-4xl mb-8 text-center font-black uppercase">
-                Password-less Login
-            </h1>
+<script setup>
+import { ref } from 'vue';
+import vm from '../main';
 
-            <form @submit.prevent="" class="bg-gray-700 max-w-md mx-auto rounded p-4">
-                <label class="block mb-2">
-                    <span class="block text-gray-400 mb-2">
-                        Enter your email address
-                    </span>
-                    <input type="email" placeholder="e.g jondoe@example.com" />
-                </label>
-            </form>
-        </section>
-    </main>
+const email = ref('');
+const isSubmitting = ref(false);
+</script>
+
+<template>
+  <main class="login min-h-screen bg-gradient-to-br from-green-500 to-cyan-500">
+    <section class="container mx-auto pt-16 pb-4 px-4">
+      <h1 class="text-4xl mb-8 text-center font-black uppercase">Password-less Login</h1>
+
+      <form @submit.prevent="" class="bg-gray-700 max-w-md mx-auto rounded p-4">
+        <label class="block mb-2">
+          <span class="block text-gray-400 mb-2"> Enter your email address </span>
+          <input
+            type="email"
+            placeholder="e.g jondoe@example.com"
+            v-model="email"
+            :disabled="isSubmitting"
+            class="block w-full rounded bg-gray-800 py-2 px-4 disabled:opacity-50"
+          />
+        </label>
+
+        <div class="flex justify-end items-center">
+          <input
+            type="submit"
+            value="Login"
+            :disabled="isSubmitting"
+            class="inline-block bg-emerald-400 text-lg text-gray-800 font-bold uppercase 
+                rounded px-4 py-2 cursor-pointer disabled:opacity-50"
+          />
+        </div>
+      </form>
+    </section>
+  </main>
 </template>
