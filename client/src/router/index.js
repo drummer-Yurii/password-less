0f2e045f1643925f2acd67748d4f630e1d42035c
Yurii Rybachok <drummer2009@ukr.net>
2023-11-10 10:47:52 +0200

    redirect to login when user not authenticated

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,7 +5,7 @@ import vm from '../main';
 const routes = [
     {
         path: '/',
-        name: 'Home',
+        name: 'home',
         component: HomeView,
         meta: {
             requiresAuth: true,
@@ -13,7 +13,7 @@ const routes = [
     },
     {
         path: '/login',
-        name: 'Login',
+        name: 'login',
         component: () => import('../views/LoginView.vue')
     }
 ]
@@ -21,6 +21,31 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach(async (to, from, next) => {
+    if (to.query.session_token) {
+        localStorage.setItem('session_token', to.query.session_token)
+        router.replace({ 'query': null })
+    }
+
+    const token = localStorage.getItem('session_token') || null
+
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (token) {
+            // Request to api to verify token
+        } else {
+            next('/login')
+            return;
+        }
+    } else if (to.matched.some(record => record.name === 'login')) {
+        if (token) {
+            // Verify token on api
+            next('/');
+            return;
+        }
+    }
+    next();
 })
 
 export default router;
